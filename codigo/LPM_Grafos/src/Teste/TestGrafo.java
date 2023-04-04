7970e801e6b39c5eec839d10a5c99ce7be75bd8c
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-04-04 10:16:55 -0300

    Update classe test

--- codigo/LPM_Grafos/src/Teste/TestGrafo.java
+++ codigo/LPM_Grafos/src/Teste/TestGrafo.java
@@ -4,24 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.LinkedList;
 
 import business.*;
 
 class TestGrafo {
 
-	@BeforeEach
-	void criarGrafo() {
-		LinkedList<Integer> vertices = new LinkedList<Integer>();
-		vertices.addLast(1);
-		vertices.addLast(3);
-		vertices.addLast(7);
-		GrafoDirecionado novoGrafo = new GrafoDirecionado("Novo Grafo");
-	}
+//	@BeforeEach
+//	void criarGrafo() throws Exception {
+//		Lista<Integer> vertices = new Lista<Integer>();
+//		vertices.add(1);
+//		vertices.add(3);
+//		vertices.add(7);
+//		GrafoDirecionado novoGrafo = new GrafoDirecionado("Novo Grafo");
+//	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCriaSubGrafo() {
+		Lista<Integer> vertices = new Lista<Integer>();
+		vertices.add(1);
+		vertices.add(3);
+		vertices.add(7);
+		
+		GrafoDirecionado novoGrafo = new GrafoDirecionado("Novo Grafo");
+		
+		GrafoDirecionado subGrafo = (GrafoDirecionado)novoGrafo.subGrafo(vertices);
+		assertEquals(1, subGrafo.existeVertice(1));
+		assertEquals(null, subGrafo.existeVertice(2));
 	}
 
 }
