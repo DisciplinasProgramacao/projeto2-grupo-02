bf65894d210a5fc641ee269a2e6272fd59512124
0xLott <llott1710@gmail.com>
2023-04-04 09:31:24 -0300

    Atualizado método carregar

--- codigo/LPM_Grafos/src/business/GrafoDirecionado.java
+++ codigo/LPM_Grafos/src/business/GrafoDirecionado.java
@@ -16,35 +16,6 @@ public class GrafoDirecionado extends GrafoMutavel {
         super(nome);
     }
 
-    /**
-     * Lê conteudo de um arquivo .txt e instancia vértices conforme descrito,
-     * adicionando-os à ABB. Em seguida, adiciona aresta divergindo do vértice
-     * de origem e convergindo no vértice de destino.
-     *
-     * @param nomeArquivo Nome do arquivo a ser lido
-     */
-    public void carregar(String nomeArquivo) throws FileNotFoundException {
-        File file = new File(nomeArquivo);
-        Scanner fileReader = new Scanner(file);
-
-        String line = null;
-        String[] tokens;
-        int key;
-
-        while (fileReader.hasNextLine()){
-            line = fileReader.nextLine();
-            tokens = line.split("\\D+");
-
-            Vertice origem = new Vertice(Integer.parseInt(tokens[0]));
-            vertices.add(Integer.parseInt(tokens[0]), origem);
-
-            Vertice destino = new Vertice(Integer.parseInt(tokens[1]));
-            vertices.add(Integer.parseInt(tokens[1]), destino);
-
-            origem.addAresta(Integer.parseInt(tokens[1]));
-        }
-        fileReader.close();
-    }
 
     /**
      * Adiciona uma aresta entre dois vértices do grafo, caso os dois vértices
