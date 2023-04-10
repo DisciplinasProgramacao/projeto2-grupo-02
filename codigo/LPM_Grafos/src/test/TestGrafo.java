c1d7e6c67364166568241b3465741fe7b61eb87e
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-04-10 20:18:34 -0300

    Update TestGrafo.java

--- codigo/LPM_Grafos/src/test/TestGrafo.java
+++ codigo/LPM_Grafos/src/test/TestGrafo.java
@@ -1,4 +1,4 @@
-package Teste;
+package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,14 +22,15 @@ class TestGrafo {
 	void testCriaSubGrafo() {
 		Lista<Integer> vertices = new Lista<Integer>();
 		vertices.add(1);
-		vertices.add(3);
-		vertices.add(7);
 		
 		GrafoDirecionado novoGrafo = new GrafoDirecionado("Novo Grafo");
+		novoGrafo.addVertice(1);
+		novoGrafo.addVertice(2);
+		novoGrafo.addAresta(1, 2, -1);
 		
-		GrafoDirecionado subGrafo = (GrafoDirecionado)novoGrafo.subGrafo(vertices);
+		
+		Grafo subGrafo = novoGrafo.subGrafo(vertices);
 		assertEquals(1, subGrafo.existeVertice(1));
-		assertEquals(null, subGrafo.existeVertice(2));
 	}
 
 }
